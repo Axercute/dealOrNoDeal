736545f70ba7448da6ec9073999eb42c9bee7025
axercute <kwokchunhei999@gmail.com>
2025-04-12 11:49:05 +0800

    DealOrNoDeal

---
+++ READ.md
@@ -0,0 +1,54 @@
+DEAL OR NO DEAL - Advanced
+
+Background:
+Deal or No Deal is a high-stakes game show where luck, risk, and strategy collide. A contestant selects one sealed briefcase from a set of 26 (or fewer, depending on the version), each containing a hidden cash amount ranging from pocket change to a life-changing jackpot. As the game progresses, the contestant eliminates the remaining cases one by one, revealing their values and narrowing down the possibilities of what's inside their own chosen case.
+
+At key points in the game, a mysterious figure known as "Bogdanoff" offers to buy the contestant's case for a certain sum based on the values still in play. The contestant must then make the ultimate decision: Deal — take the Banker's offer and walk away, or No Deal — keep playing in hopes of a bigger reward.
+
+It’s a thrilling test of nerve and intuition, where every decision could mean walking away with thousands—or just a few dollars.
+
+Idea: 
+Deal or No Deal was originally created by Dick de Rijk, a Dutch television producer and writer. He developed the show in 2000 in partnership with Endemol, a Dutch production company known for creating international hit formats.
+
+The show first aired in the Netherlands as Miljoenenjacht ("Hunt for Millions") and became a massive success. Its unique format was quickly adapted in countries around the world, with local hosts and tweaks to fit regional tastes. One of the most famous versions is the U.S. edition, hosted by Howie Mandel, which premiered in 2005.
+![alt text](image.png)
+
+How to play:
+Traditional:
+game_prize= [0.01,1,5,10,25,50,75,100,200,300,400,500,750,1000,5000,10000,25000,50000,75000,100000,200000,300000,400000,500000,750000,1000000] //26 cases
+
+Game schedule:
+Player picks a case for himself
+
+Banker will offer every few cases opened
+Every few cases opened are [6,5,4,3,2,1,1,1,1,1]
+
+Last choice depend if the player wants to switch the final case
+
+How to play:
+Axercute_style:
+game_prize= [1,5,10,15,30,50,75,100,150,250,500,1000,1500,3000,5000,7500,10000,15000,25000,50000,100000,150000,300000,50000,1000000] //25 cases
+
+Game schedule:
+Player picks a case for himself 
+
+Banker will offer every few cases opened
+Every few cases opened are [5,5,5,3,3,2]
+
+Future development:
+
+Emojis used as deal or no deal lady will be randomized:
+🫃🫃🏻🫃🏼🫃🏽🫃🏾🫃🏿🫄🫄🏻🫄🏼🫄🏽🫄🏿🫄🏾
+
+Case design:
+one inner box, one outer box with grid separated in 3, middle with a circle.
+
+Animation:
+Case clicked before and after:
+![alt text](image-1.png)
+![alt text](image-2.png)
+
+Challenges:
+Setting up javascript function on creating element.
+Making mistake of not making them a class first but ID
+Mistake of not setting saved value for the generate function
---
+++ script.js
@@ -0,0 +1,130 @@
+let ladyRandom=["🫃","🫃🏻","🫃🏼","🫃🏽","🫃🏾","🫃🏿","🫄","🫄🏻","🫄🏼","🫄🏽","🫄🏿","🫄🏾"];
+let prizeMoney=[1,5,10,15,30,50,75,100,150,250,500,1000,1500,3000,5000,7500,10000,15000,25000,50000,100000,150000,300000,500000,1000000]
+let turn = 0;
+let yourPrize;
+let dealerOffer;
+//----------------Cached-------------------
+lady = document.querySelector(".lady");
+Xinner_design = document.querySelector(".inner_design")
+Xdesign=document.querySelector(".design")
+
+
+//---------Display-------------------------
+lady.innerText=ladyRandom[Math.floor((Math.random()*ladyRandom.length))] //test for first lady
+
+
+//---------Function------------------------
+function dealorNodeal(){
+    dealerOffer=Math.round(prizeMoney.reduce((startElement,nextElement)=>{return (startElement+nextElement)/(prizeMoney.length+1)},yourPrize))
+setTimeout(() => {
+window.alert(`Dealer offers $${dealerOffer}`)
+}, 1);  //wait for last number to load
+}
+
+function createwholeSet(){ //making a function to place inside body
+//declaring we are putting wholeSet into main
+const main = document.querySelector("main")
+const wholeSet = document.createElement("div")
+wholeSet.setAttribute("class","whole_set")
+main.appendChild(wholeSet)
+//Everything here is the same as:
+//<div class="whole_set">
+//<div id="inner_design">$500,000</div>
+//<div id="design">
+//    <div id="row1_design"></div>
+//    <div id="row2_design">
+//        <div id="button_design"></div>
+//    </div>
+//    <div id="row3_design">
+//    </div>
+//</div>
+//<div class="lady"></div>
+//</div>
+
+const inner_design=document.createElement("div")
+inner_design.setAttribute("class","inner_design")
+wholeSet.appendChild(inner_design)
+
+const design = document.createElement("div")
+design.setAttribute("class","design")
+wholeSet.appendChild(design)
+
+const row1_design=document.createElement("div")
+row1_design.setAttribute("id","row1_design")
+design.appendChild(row1_design)
+
+const row2_design=document.createElement("div")
+row2_design.setAttribute("id","row2_design")
+design.appendChild(row2_design)
+
+const button_design = document.createElement("div")
+button_design.setAttribute("id","button_design")
+row2_design.appendChild(button_design)
+
+const row3_design=document.createElement("div")
+row3_design.setAttribute("id","row3_design")
+design.appendChild(row3_design)
+
+const lady = document.createElement("div")
+lady.setAttribute("class","lady")
+lady.innerText=ladyRandom[Math.floor((Math.random()*ladyRandom.length))]
+design.after(lady)
+//--------------------same as the one outside----------------------------
+design.addEventListener("click",()=>{ 
+    design.setAttribute("id","clicked")
+    turn+=1
+//--------------------same as the one outside----------------------------
+rngPrize = Math.floor((Math.random()*prizeMoney.length)) //rng and store it
+generatedPrize=prizeMoney[rngPrize] //store the generatedPrize
+inner_design.innerText=`$${generatedPrize.toLocaleString()}` //generating
+prizeMoney.splice(rngPrize,1) //use generated rng and remove that index
+if(turn===1){
+    yourPrize=generatedPrize
+    inner_design.innerText='You chose this box'
+}
+if(turn===6){
+    dealorNodeal()
+    }
+if(turn===11){
+    dealorNodeal()
+    }
+if(turn===16){
+    dealorNodeal()
+    }
+if(turn===19){
+    dealorNodeal()
+    }
+if(turn===22){
+    dealorNodeal()
+    }
+if(turn===24){
+    dealorNodeal()
+    }
+})
+
+}
+//repeat this for 24 times as we have one default. 
+for(let i=0;i<24;i++){
+    createwholeSet()
+}
+
+
+//---------AddEventListener-----------------
+Xdesign.addEventListener("click",()=>{ //test for first lady.
+    Xdesign.setAttribute("id","clicked")
+    turn+=1
+//randomized prizeMoney into the box
+XrngPrize = Math.floor((Math.random()*prizeMoney.length)) //rng and store it
+XgeneratedPrize=prizeMoney[XrngPrize] //store the generatedPrize
+Xinner_design.innerText=`$${prizeMoney[XrngPrize].toLocaleString()}` //generating
+prizeMoney.splice(XrngPrize,1) //use generated rng and remove that index
+if(turn===1){
+    yourPrize=XgeneratedPrize
+    Xinner_design.innerText='You chose this box'
+}
+if(turn===9){
+dealerOffer=prizeMoney.reduce((startElement,nextElement)=>{return (startElement+nextElement)/(prizeMoney.length-1)},yourPrize)
+window.alert(`Dealer offers $${dealerOffer}`)
+}
+})
+//----------Game Start----------------------
